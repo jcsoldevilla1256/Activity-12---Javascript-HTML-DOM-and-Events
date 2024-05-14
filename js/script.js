4e8366936efd0502f6c25bec8e2888457e30dfd4
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-05-14 14:37:47 +0800

    Program add task button to add task written in placeholder

--- js/script.js
+++ js/script.js
@@ -1,4 +1,23 @@
 document.querySelector("#addTask").addEventListener("click", () => {
-    // document.querySelector("#taskInput").value;
-    document.querySelector("#taskArea").innerHTML += '<p>New task!</p>';
+    document.querySelector("#taskArea").innerHTML += 
+    `<hr />
+    <div class="flex">
+        <div class="left">
+            <div class="pe-1">
+                <button class="btn btn-success p-1 pe-2 ps-2">
+                    <small><i class="fa-solid fa-check"></i></small>
+                </button>
+            </div>
+            <div class="pe-3">
+                <button class="btn btn-danger p-1 ps-2 pe-2">
+                    <small><i class="fa-solid fa-trash"></i></small>
+                </button>
+            </div>
+        </div>
+        <div class="right">
+            <p class="pb-0 mb-0">${document.querySelector("#taskInput").value}</p>
+        </div>
+    </div>`;
+    document.querySelector("#taskInput").value = "";
 });
+
