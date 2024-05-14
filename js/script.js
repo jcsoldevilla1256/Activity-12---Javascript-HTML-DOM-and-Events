4df28942e0d14633b49eb935a401a78f90e52865
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-05-14 13:19:35 +0800

    Edit header

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.querySelector("#addTask").addEventListener("click", () => {
+    // document.querySelector("#taskInput").value;
+    document.querySelector("#taskArea").innerHTML += '<p>New task!</p>';
+});
