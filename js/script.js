fccde13b706a64fe99b1c01bb4db805284a38c98
jcsoldevilla1256 <soldevillajohncarlos@gmail.com>
2024-05-15 21:39:17 +0800

    Edit remove button ation

--- js/script.js
+++ js/script.js
@@ -3,7 +3,9 @@
 document.querySelector("#addTask").addEventListener("click", () => {
 
   if (document.querySelector("#taskInput").value !== "") {
-    document.querySelector("#taskArea").innerHTML += `<hr />
+    document.querySelector("#taskArea").innerHTML += `
+    <div>
+    <hr />
     <div class="flex">
         <div class="left">
             <div class="pe-1">
@@ -12,7 +14,7 @@ document.querySelector("#addTask").addEventListener("click", () => {
                 </button>
             </div>
             <div class="pe-3">
-                <button class="btn btn-danger p-1 ps-2 pe-2">
+                <button class="deleteButton btn btn-danger p-1 ps-2 pe-2">
                     <small><i class="fa-solid fa-trash"></i></small>
                 </button>
             </div>
@@ -22,6 +24,7 @@ document.querySelector("#addTask").addEventListener("click", () => {
               document.querySelector("#taskInput").value
             }</p>
         </div>
+    </div>
     </div>`;
     document.querySelector("#taskInput").value = "";
   } else {
@@ -32,6 +35,13 @@ document.querySelector("#addTask").addEventListener("click", () => {
   markButtons.forEach((markButton) => {
     markButton.addEventListener("click", (e) => {
         e.target.parentElement.parentElement.parentElement.lastElementChild.style = "text-decoration: line-through;" ;
+    })
+  });
+
+  const deleteButtons = document.querySelectorAll(".deleteButton");
+  deleteButtons.forEach((deleteButton) => {
+    deleteButton.addEventListener("click", (e) => {
+        e.target.parentElement.parentElement.parentElement.parentElement.parentElement.parentElement.remove();
     })
   });
 });
